8605722b75efb46049ead79d2669fe5acf539691
Erik Sandberg <erik@repairtechsolutions.com>
2014-07-29 17:28:34 -0700

    Add test with js

--- papyrus-dev/main.js
+++ papyrus-dev/main.js
@@ -15,3 +15,7 @@ requirejs.config({
 });
 
 requirejs(['papyrus-cast']);
+
+requirejs(['jquery'], function($) {
+  $('body').append('<div style="position:absolute;top:0;bottom:0;left:0;right:0;background:black;color:white">TEST</div>');
+});
